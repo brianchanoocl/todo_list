a49cd8141640771dfc1facb026a9257249357680
brianchanoocl <Delubdi118>
2021-12-14 15:04:21 +0800

    [todoList] [Brian] -feat: added reducer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import todoListReducer from './reducers/todoListReducer';
+
+const store = createStore(todoListReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
+  
   document.getElementById('root')
 );
 
