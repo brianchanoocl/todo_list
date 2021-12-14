33f6058a8e7a3ee865c87e572dc54a479f944877
brianchanoocl <Delubdi118>
2021-12-14 15:21:20 +0800

    [todoList] [Brian] -feat: implemented code such that todoGroup can get state and display items in state

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,15 @@
-import { useState } from 'react';
+import { useSelector } from 'react-redux';
 import TodoGenerator from './TodoGenerator';
 import TodoGroup from './TodoGroup';
 
 function TodoList() {
-    const [todoItems, settodoItems] = useState([]);
-    
-    function handleAddTodoItem(todoItemDetail){
-        settodoItems([...todoItems, todoItemDetail]);
-    }
+    const todoItems = useSelector(state => state.todoItems);
 
     return(
         <div className="main-page">
             <h1 className="center-text">Todo List</h1>
             <TodoGroup todoItems={todoItems}></TodoGroup>
-            <TodoGenerator handleAddTodoItem={handleAddTodoItem}></TodoGenerator>
+            <TodoGenerator /*handleAddTodoItem={handleAddTodoItem}*/></TodoGenerator>
         </div>
     );
 }
